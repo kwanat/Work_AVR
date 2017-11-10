5c273c3a6acb236e261274b894c89a49ba7a9767
kwanat <wanat.kamil@wp.pl>
2017-11-10 14:03:16 +0100

    'christmas shop' project started

---
+++ christmas_shop/peripheral_config.h
@@ -0,0 +1,42 @@
+/*
+ * peripheral_config.h
+ *
+ *  Created on: 10 lis 2017
+ *      Author: Kamil
+ */
+
+#ifndef PERIPHERAL_CONFIG_H_
+#define PERIPHERAL_CONFIG_H_
+
+#define PROGRAM_TIME 15
+#define SEQUENCE_TIME 20
+
+
+
+# define DDR_LANTERN DDRA
+# define PORT_LANTERN PORTA
+
+# define DDR_STATIC_LED DDRA
+# define PORT_STATIC_LED PORTA
+
+#define DDR_LED DDRA
+#define PORT_LED PORTA
+
+#define PINKEY PINB
+#define PORTKEY PORTB
+
+
+
+#define LANTERN1 (1<<PA6)
+#define LANTERN2 (1<<PA7)
+
+#define STATIC_LED1 (1<<PA4)
+
+#define LED1 (1<<PA0)
+#define LED2 (1<<PA1)
+#define LED3 (1<<PA2)
+#define LED4 (1<<PA3)
+
+#define KEY (1<<PB0)
+#define KEY1 (1<<PB1)
+#endif /* PERIPHERAL_CONFIG_H_ */
